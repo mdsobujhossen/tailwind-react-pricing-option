cc513b319138173b413c9abd911ebe3e479bfd8d
mdsobujhossen <itmdsobuj@gmail.com>
2026-03-24 17:11:52 +0600

    started

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
 
 const navigaitonData = [
     {
@@ -30,12 +31,22 @@ const navigaitonData = [
 ];
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false)
+    const links = navigaitonData.map((route) => <Link key={route.id} route={route}></Link>)
     return (
-        <div>
-            <ul className='flex gap-3'>
-                {
-                    navigaitonData.map((route) => <Link key={route.id} route={route}></Link>)
-                }
+        <div className='flex justify-between mx-5' onClick={() => setOpen(!open)}>
+            <span className='flex gap-3'>
+                {open? <X></X>: <Menu className='md:hidden'></Menu>}
+                <ul className={`
+                    md:hidden absolute duration-1000
+                    ${open? 'top-6' : '-top-40'} 
+                    bg-amber-200 `}>
+                    {links}
+                </ul>
+                <h3>My nav</h3>
+            </span>
+            <ul className='hidden md:flex gap-3'>
+                {links}
             </ul>
             {/* <ul className='flex gap-3'>
                 {
@@ -47,6 +58,7 @@ const NavBar = () => {
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">contact</a></li>
             </ul> */}
+            <button>sign in</button>
         </div>
     );
 };
